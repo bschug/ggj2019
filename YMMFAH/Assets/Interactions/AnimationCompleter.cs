7871376096857226be20c61c3e5da723f53fd2ec
James Coote <coote.james@gmail.com>
2019-01-26 17:31:41 +0100

    Implemented interaction and animation end triggers. Also implemented Recieve animations

---
+++ YMMFAH/Assets/Interactions/AnimationCompleter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationCompleter : MonoBehaviour
+{
+    void Start()
+    {
+
+        Animator animation = gameObject.GetComponent<Animator>();
+        if (animation != null)
+        {
+            AnimationEvent animationEvent = new AnimationEvent();
+            animationEvent.time = 1f;
+            animationEvent.functionName = "InvokeOnCompleteSelf";
+
+            animation.runtimeAnimatorController.animationClips[0].AddEvent(animationEvent);
+        }        
+    }
+
+    [Header("On Complete")]
+
+    public UnityEngine.Events.UnityEvent onComplete;
+
+    public void InvokeOnCompleteSelf(int irrelevant)
+    {
+        if (onComplete != null)
+        {
+            onComplete.Invoke();
+        }
+    }
+}
---
+++ YMMFAH/Assets/Interactions/InteractionCompleter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionCompleter : MonoBehaviour
+{
+    public InteractionManager parentInteractionManager;
+
+    public void InvokeOnComplete(int irrelevant)
+    {
+        if (parentInteractionManager.onComplete != null)
+        {
+            parentInteractionManager.onComplete.Invoke();
+        }
+    }
+}
